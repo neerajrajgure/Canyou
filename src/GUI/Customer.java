986ed04d8da8ac2e17399ffbcb17bed1539af845
unknown <sheetalrajgure@gmail.com>
2015-12-13 21:28:56 -0500

    User registration

---
+++ src/GUI/Customer.java
@@ -0,0 +1,16 @@
+package GUI;
+
+import java.sql.Date;
+
+public class Customer {
+    long cid;
+    String FName;
+    String LName;
+    String address;
+    String phone;
+    long phoneNum;
+    String emailid;
+    Date DOB;
+    char flag;   
+
+}
