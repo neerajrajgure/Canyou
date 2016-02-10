13555f0245351e3c3d0d6dcdb0651a1fee3e9c91
Neeraj Rajgure <neeraj@tumblr.com>
2016-02-09 21:47:35 -0500

    Fixed customer registration update db code.

--- src/GUI/CustRegForm.java
+++ src/GUI/CustRegForm.java
@@ -1,7 +1,5 @@
 package GUI;
 
-
-
 import java.awt.*;
 import java.applet.*;
 import java.awt.event.*;
@@ -17,7 +15,7 @@ import java.sql.ResultSet;
 import java.awt.Dialog.ModalExclusionType;
 import javax.swing.JButton;
 
-public class CustRegForm extends JDialog implements ActionListener{
+public class CustRegForm extends JDialog {
     private static CafeBill _cb;
     public CustRegForm(CafeBill cb){
         _cb = cb;
@@ -41,9 +39,9 @@ public class CustRegForm extends JDialog implements ActionListener{
     TextField firstname=new TextField();
     TextField lastname=new TextField();
     TextField phoneno=new TextField();
-    TextField dobyyyy=new TextField();
-    TextField dobmm=new TextField();
     TextField dobdd=new TextField();
+    TextField dobmm=new TextField();
+    TextField dobyyyy=new TextField();
     TextField email=new TextField();
 
 
@@ -58,20 +56,20 @@ public class CustRegForm extends JDialog implements ActionListener{
         setDefaultCloseOperation(JDialog.DO_NOTHING_ON_CLOSE);
     }
     public long getNextCid() {
-        //connectDatabase();
-        long maxcid=0;
+        long currentMaxCid=0;
         try {
+            connect = DriverManager.getConnection(CafeBill.hmsDbUrl);
             preparedStatement = connect.prepareStatement("SELECT MAX(cid) FROM customer");
             ResultSet rs=preparedStatement.executeQuery();
             while(rs.next())
             {
-                maxcid=rs.getInt(1);
+                currentMaxCid=rs.getInt(1);
             }
         } catch (SQLException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
-        return ++maxcid;
+        return ++currentMaxCid;
     }
 
     /*  public void paint(Graphics g)
@@ -79,90 +77,83 @@ public class CustRegForm extends JDialog implements ActionListener{
        //g.drawString(msg,200,450);
     }*/
 
-    public void actionPerformed(ActionEvent ae)
+    public boolean addCustomer()
     {
-        if(ae.getActionCommand().equals("Submit"))
-        {
-            try {
-                String doby = dobyyyy.getText();
-                String dobm = dobmm.getText();
-                String dobd = dobdd.getText();
-                String dobStr1 = doby + "-" + dobm + "-" + dobd;
-                System.out.println("DOB in (YYYY-MM-DD) format: " + dobStr1 );
-                String dobStr2 = dobd + "-" + dobm + "-" + doby;
-                System.out.println("DOB in reverse format: " + dobStr2 );
-                Class.forName("com.mysql.jdbc.Driver");
-                // Setup the connection with the DB
-                connect = DriverManager.getConnection(CafeBill.hmsDbUrl);
-                String query="Insert into customer (cid,FName,LName,Address,phonenum,phone,emailid,DOB,flag) values (?,?,?,?,?,?,?,?,?)";
-                preparedStatement=connect.prepareStatement(query);
-                long newCid = getNextCid();
-                System.out.println("Customer will get this Cid: " + newCid );
-                preparedStatement.setLong(1, newCid);
-                //                preparedStatement.setInt(2, customerId);
-                preparedStatement.setString(2, firstname.getText());
-                preparedStatement.setString(3, lastname.getText());
-                preparedStatement.setString(4, "");
-                preparedStatement.setString(5,  phoneno.getText());
-                preparedStatement.setInt(6, 12345);
-                preparedStatement.setString(7, email.getText());
-                preparedStatement.setString(8,  dobStr2);
-                preparedStatement.setString(9, "");
-                preparedStatement.executeUpdate();
-                preparedStatement.close();
-                System.out.println("Customer Test updated " );
+        try {
+            long newCid = getNextCid();
+            if(firstname.getText().trim().length() <= 0) {
+                return false;
             }
 
-            /*
-            try {
-            Class.forName("com.mysql.jdbc.Driver");
+            String dobd = dobdd.getText();
+            String dobm = dobmm.getText();
+            String doby = dobyyyy.getText();
+
+            if(doby.isEmpty() && dobm.isEmpty() && dobd.isEmpty()) {
+                dobd = "1";
+                dobm = "1";
+                doby = "1901";
+            }
+            else if(doby.isEmpty() || dobm.isEmpty() || dobd.isEmpty()) {
+                JOptionPane.showMessageDialog ( null, "Date format error. Either all date fields have to be present or none.", "Date format", JOptionPane.ERROR_MESSAGE);
+
+                return false;
+            }
+
+            String dobStr = doby + "-" + dobm + "-" + dobd;
+            System.out.println("DOB in (YYYY-MM-DD) format: " + dobStr);
             // Setup the connection with the DB
             connect = DriverManager.getConnection(CafeBill.hmsDbUrl);
-            String query="Insert into customer (FName,LName,phone,emailid,DOB) values (?,?,?,?,?)";
+            String query="Insert into customer (cid,FName,LName,Address,phonenum,phone,emailid,DOB,flag) values (?,?,?,?,?,?,?,?,?)";
             preparedStatement=connect.prepareStatement(query);
-            preparedStatement.setString(1, firstname.getText() );
-            preparedStatement.setString(2, lastname.getText() );
-            preparedStatement.setString(3, phoneno.getText() );
-            preparedStatement.setString(4, dob.getText() );
-            preparedStatement.setString(5, email.getText() );
+            System.out.println("Customer will get this Cid: " + newCid );
+            preparedStatement.setLong(1, newCid);
+            preparedStatement.setString(2, firstname.getText());
+            preparedStatement.setString(3, lastname.getText());
+            preparedStatement.setString(4, "Address - 1"); // No address asked yet
+            preparedStatement.setString(5,  phoneno.getText());
+            try {
+                if(phoneno.getText().trim().isEmpty()) {
+                    preparedStatement.setLong(6, 1234567890);
+                }
+                else {
+                    preparedStatement.setLong(6, new Long(phoneno.getText()).longValue());
+                }
             }
-             */
-            catch (ClassNotFoundException e2) {
-                // TODO Auto-generated catch block
-                e2.printStackTrace();
+            catch ( NumberFormatException nfe ) {
+                preparedStatement.setLong(6, 1234567890);
+                nfe.printStackTrace();
             }
-            catch (SQLException e2) {
-                // TODO Auto-generated catch block
-                e2.printStackTrace();
-            }
-            JOptionPane.showMessageDialog(this, "Customer Data Saved!");
-            System.out.println("Text Submit" );
-            //this.dispose();
-            firstname.setText(" ");
-            lastname.setText(" ");
-            phoneno.setText(" ");
-            dobyyyy.setText(" ");
-            dobmm.setText(" ");
-            dobdd.setText(" ");
-            email.setText(" ");
+            preparedStatement.setString(7, email.getText());
+            preparedStatement.setString(8,  dobStr);
+            preparedStatement.setString (9, "C"); // C - Create, D - Deleted
+            preparedStatement.executeUpdate();
         }
-        if(ae.getActionCommand().equals("Clear"))
-        {
-            firstname.setText(" ");
-            lastname.setText(" ");
-            phoneno.setText(" ");
-            dobyyyy.setText(" ");
-            dobmm.setText(" ");
-            dobdd.setText(" ");
-            email.setText(" ");
-            System.out.println("Text Cleared" );
+        catch (SQLException e2) {
+            // TODO Auto-generated catch block
+            e2.printStackTrace();
+
+            return false;
         }
+        clearFields();
+
+        return true;
     }
 
+    public void clearFields() {
+        firstname.setText("");
+        lastname.setText("");
+        phoneno.setText("");
+        dobyyyy.setText("");
+        dobmm.setText("");
+        dobdd.setText("");
+        email.setText("");
+        System.out.println("Text Cleared" );
+    }
 
     public static void main(String g[])
     {
-
+        new  CustRegForm().setVisible(true);
     }
 
     private void createAndShowGUI() {
@@ -182,17 +173,36 @@ public class CustRegForm extends JDialog implements ActionListener{
         getContentPane().add(phoneno);
 
         getContentPane().add(d_o_b);
-        getContentPane().add(dobyyyy);
-        getContentPane().add(dobmm);
         getContentPane().add(dobdd);
+        getContentPane().add(dobmm);
+        getContentPane().add(dobyyyy);
 
         getContentPane().add(email_id);
         getContentPane().add(email);
 
         getContentPane().add(b1);
         getContentPane().add(b2);
-        b1.addActionListener(this);
-        b2.addActionListener(this);
+        // b1.addActionListener(this);
+        b1.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+
+                if(addCustomer() == true) {
+                    System.out.println("Customer sucessfully added." );
+                    JOptionPane.showMessageDialog(null, "Customer Data Saved!");
+                    dispose();
+               }
+                else {
+                    System.out.println("Error: Failed to save customer info." );
+                    JOptionPane.showMessageDialog(null, "Error: Unable to save customer data.", "Error", JOptionPane.ERROR_MESSAGE);
+                }
+            }
+        });
+        // b2.addActionListener(this);
+        b2.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                clearFields();
+            }
+        });
 
         first_name.setBounds(25,65,90,20);
         last_name.setBounds(25,90,90,20);
@@ -204,9 +214,9 @@ public class CustRegForm extends JDialog implements ActionListener{
         firstname.setBounds(140,65,170,20);
         lastname.setBounds(140,90,170,20);
         phoneno.setBounds(140,120,170,20);
-        dobyyyy.setBounds(140,150,50,20);
+        dobdd.setBounds(140,150,50,20);
         dobmm.setBounds(196,150,50,20);
-        dobdd.setBounds(252,150,50,20);
+        dobyyyy.setBounds(252,150,50,20);
         email.setBounds(140,180,170,20);
         b1.setBounds(140,210,70,20);
         b2.setBounds(220,210,70,20);
