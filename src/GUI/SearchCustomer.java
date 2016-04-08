2e4028ad68446f88dfae8a1d55eda8f156c05048
SaNKeT <SaNKeT>
2016-04-09 00:21:40 +0530

    Customer Lookup

--- src/GUI/SearchCustomer.java
+++ src/GUI/SearchCustomer.java
@@ -19,9 +19,11 @@ import javax.swing.JOptionPane;
 import javax.swing.JTable;
 import java.util.Vector;
 import javax.swing.JButton;
-public class SearchCustomer extends JDialog implements ActionListener{
+public class SearchCustomer extends JDialog implements ActionListener
+{
     private static CafeBill _cb;
-    public SearchCustomer(CafeBill cb){
+    public SearchCustomer(CafeBill cb)
+    {
         _cb = cb;
     }
     private static Connection connect = null;
@@ -39,11 +41,9 @@ public class SearchCustomer extends JDialog implements ActionListener{
     final static String COLUMN_EMAIL = "EMAILID";
     final static String COLUMN_CID = "CID";
     final static String COLUMN_ADDRESS = "ADDRESS";
-
-    // ArrayList<search_info> searchlist = new ArrayList<search_info>();
-    // Vector<search_info> searchlist = new Vector<search_info>();
-    Vector<Vector> searchlist = new Vector<Vector>();
+    String quryPart1;
     
+    Vector<Vector> searchlist = new Vector<Vector>();
     ArrayList<CustInfo> searchlist1 = new ArrayList<CustInfo>();
     Vector<CustInfo> searchData = new Vector<CustInfo>();
     
@@ -60,11 +60,9 @@ public class SearchCustomer extends JDialog implements ActionListener{
     TextField firstname=new TextField();
     TextField lastname=new TextField();
     TextField phoneno=new TextField();
-    TextField dobyyyy=new TextField();
-    TextField dobmm=new TextField();
-    TextField dobdd=new TextField();
-    TextField email=new TextField();
-    private final JButton btnNewButton = new JButton("Close");
+	TextField dobTxt=new TextField();
+	TextField email=new TextField();
+    private final Button btnNewButton = new Button("Close");
 
     public SearchCustomer()
     {
@@ -75,7 +73,7 @@ public class SearchCustomer extends JDialog implements ActionListener{
         setDefaultCloseOperation(JDialog.DO_NOTHING_ON_CLOSE);
         setTitle("Search Customer");
         createAndShowGUI();
-        addWindowListener(new myWindowAdapter1());
+        addWindowListener(new WindowListener());
         setBackground(Color.LIGHT_GRAY);
         setForeground(Color.black);
         getContentPane().setLayout(null);
@@ -90,11 +88,8 @@ public class SearchCustomer extends JDialog implements ActionListener{
         getContentPane().add(phoneno);
 
         getContentPane().add(d_o_b);
-        getContentPane().add(dobyyyy);
-        getContentPane().add(dobmm);
-        getContentPane().add(dobdd);
-
-        getContentPane().add(email_id);
+		getContentPane().add(dobTxt);
+		getContentPane().add(email_id);
         getContentPane().add(email);
 
         getContentPane().add(btnSearch);
@@ -112,9 +107,7 @@ public class SearchCustomer extends JDialog implements ActionListener{
         firstname.setBounds(120,65,170,20);
         lastname.setBounds(120,90,170,20);
         phoneno.setBounds(120,120,170,20);
-        dobyyyy.setBounds(120,150,50,20);
-        dobmm.setBounds(180,150,50,20);
-        dobdd.setBounds(250,150,50,20);
+        dobTxt.setBounds(120,150,170,20);
         email.setBounds(120,180,170,20);
         btnSearch.setBounds(120,210,70,20);
         btnClear.setBounds(207,210,70,20);
@@ -127,38 +120,23 @@ public class SearchCustomer extends JDialog implements ActionListener{
         getContentPane().add(btnNewButton);
         //getContentPane().setDefaultButton(btnSearch);
     }
-    /*  public void paint(Graphics g)
-    {
-       //g.drawString(msg,200,450s);
-    }*/
-    public void actionPerformed(ActionEvent ae)
+    @SuppressWarnings("unchecked")
+	public void actionPerformed(ActionEvent ae)
     {
         if(ae.getActionCommand().equals("Search"))
         {
             String fname = firstname.getText();
             String lname = lastname.getText();
             String phno = phoneno.getText();
-            String doby = dobyyyy.getText();
-            String dobm = dobmm.getText();
-            String dobd = dobdd.getText();
+            String dob = dobTxt.getText();
+
             String emal = email.getText();
             System.out.println(" firstname from Search customer gui : "+fname);
             System.out.println(" LastName from Search customer gui : "+lname);
             System.out.println(" PHONENO from Search customer gui :"+phno);
-            System.out.println(" DOB year from Search customer gui : "+doby);
-            System.out.println(" DOB nonth from Search customer gui : "+dobm);
-            System.out.println(" DOB date from Search customer gui :"+dobd);
+            System.out.println(" Date of birth from Search customer gui : "+dob);
             System.out.println(" Email from Search customer gui : "+emal);
-            String dobStr1 = doby + "-" + dobm + "-" + dobd;
-            System.out.println("DOB in (YYYY-MM-DD) format: " + dobStr1 );
-            String dobStr2 = dobd + "-" + dobm + "-" + doby;
-            System.out.println("DOB in reverse format: " + dobStr2 );
 
-            //			if (fname.length() > 0) {
-            //				something1 = CONSTANT.TABLE_COL_NAME;
-            //				something2 = fname;
-            //			}
-            
             try {
                 Class.forName("com.mysql.jdbc.Driver");
                 // Setup the connection with the DB
@@ -186,11 +164,6 @@ public class SearchCustomer extends JDialog implements ActionListener{
                     queryParamName = COLUMN_L_NAME;
                     queryParamVal = lastname.getText();
                 }
-                else if (dobStr2.length() > 0) //DOB
-                {
-                    queryParamName = COLUMN_DOB;
-                    queryParamVal = dobStr2;
-                }
                 else if( phno.length() > 0 )
                 {
                     queryParamName = COLUMN_PHONENUM;
@@ -200,8 +173,6 @@ public class SearchCustomer extends JDialog implements ActionListener{
                 System.out.println("Query for cust search: " + queryCustSearch);
                 preparedStatement = connect.prepareStatement(queryCustSearch);
                 ResultSet rs = preparedStatement.executeQuery();
-                //search_info si = new search_info();
-                
                 while(rs.next())
                 {
                     // "cid", "Fname", "Lname", "Address", "phonenum", "phone", "emailid", "DOB", "flag"
@@ -219,7 +190,8 @@ public class SearchCustomer extends JDialog implements ActionListener{
                     si.setEmailid(rs.getString(COLUMN_EMAIL));
                     // searchlist.add(si);
                     // searchlist.size();
-                    Vector searchData = new Vector();
+                    @SuppressWarnings("rawtypes")
+					Vector searchData = new Vector();
                     searchData.add(si.getCid());
                     searchData.add(si.getFName());
                     searchData.add(si.getLName());
@@ -237,68 +209,17 @@ public class SearchCustomer extends JDialog implements ActionListener{
                 // TODO Auto-generated catch block
                 e.printStackTrace();
             }
-
-            System.out.println(" vector length is :" + searchlist.size());
-            dispose();
-            CustomerRetrievalForm custRetrieval = new CustomerRetrievalForm(searchlist);
-            custRetrieval.setVisible(true);
-
-            /*
-	            // list iterate
-                Iterator<search_info> iter=searchlist.iterator();
-                search_info sn = new search_info();
-                while(iter.hasNext())
-                {
-                search_info s = iter.next();
-                   if((fname.equals(si.getFName())) || (lname.equals(si.getLName())) || (phno.equals(si.getPhonenum())) || (doby.equals(si.getDOB())) || ( dobm.equals(si.getDOB()))  ||  ( dobd.equals(si.getDOB())) || ( emal.equals(si.getEmailid())))
-                   {
-                       sn.FName = si.FName;
-                       sn.LName = si.LName;
-                       sn.Phonenum = si.Phonenum;
-                       sn.DOB =si .DOB;
-                       sn.emailid = si.emailid;
-                       searchlist1.add(sn);
-                       checksearchResult=true;
-                   }
-                }
-                if(!checksearchResult)
-                {
-                JOptionPane.showMessageDialog(null, "No results found", "MESSAGE", JOptionPane.INFORMATION_MESSAGE);
-                }
-                else
-                {
-                Iterator<search_info> iter1=searchlist1.iterator();
-                //iterate and add to Jtable
-                while(iter1.hasNext()){
-                search_info sa = iter.next();
-                System.out.println( iter1.next() );
-                }
-               }
-            }
-            catch (SQLException e) {
-           // TODO Auto-generated catch block
-            e.printStackTrace();
-            }
-
-			//new CustomerReterivalForm().setVisible(true);
-		}
-             */
         }
-        else
-            if(ae.getActionCommand().equals("Clear"))
-            {
-                firstname.setText(" ");
-                lastname.setText(" ");
-                phoneno.setText(" ");
-                dobyyyy.setText(" ");
-                dobmm.setText(" ");
-                dobdd.setText(" ");
-                email.setText(" ");
-                System.out.println("Text Cleared" );
-            }
+        else if(ae.getActionCommand().equals("Clear"))
+        {
+        	firstname.setText(" ");
+        	lastname.setText(" ");
+        	phoneno.setText(" ");
+        	dobTxt.setText(" ");
+        	email.setText(" ");
+        	System.out.println("Text Cleared" );
+        }
     }
-
-
     public static void main(String g[])
     {
         SearchCustomer scf=new SearchCustomer();
@@ -316,8 +237,6 @@ public class SearchCustomer extends JDialog implements ActionListener{
             // This will load the MySQL driver, each DB has its own driver
             preparedStatement = connect.prepareStatement(query);
             resultSet = preparedStatement.executeQuery();
-
-
             //displayResultSet(resultSet);
         } catch (Exception e) {
             e.printStackTrace();
