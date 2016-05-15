394a2c94cea6fa4948e323f92b84ea52b2031d15
SaNKeT <SaNKeT>
2016-05-16 01:07:26 +0530

    Upding old menu pricing and adding new menu items.

---
+++ src/db_scripts/SQLScript_patch_4.sql
@@ -0,0 +1,84 @@
+#Update old items for price.
+
+UPDATE item SET price = 32 WHERE itemId = 1001;
+UPDATE item SET price = 40 WHERE itemId = 1010;
+UPDATE item SET price = 55 WHERE itemId = 1012;
+UPDATE item SET price = 49 WHERE itemId = 1014;
+UPDATE item SET price = 64 WHERE itemId = 1016;
+UPDATE item SET price = 35 WHERE itemId = 1002;
+UPDATE item SET price = 48 WHERE itemId = 1004;
+UPDATE item SET price = 55 WHERE itemId = 1006;
+UPDATE item SET price = 48 WHERE itemId = 1008;
+UPDATE item SET price = 39 WHERE itemId = 1020;
+UPDATE item SET price = 45 WHERE itemId = 1021;
+UPDATE item SET price = 47 WHERE itemId = 1022;
+UPDATE item SET price = 45 WHERE itemId = 1023;
+UPDATE item SET price = 55 WHERE itemId = 1024;
+UPDATE item SET price = 62 WHERE itemId = 1025;
+UPDATE item SET price = 65 WHERE itemId = 1026;
+UPDATE item SET price = 45 WHERE itemId = 1027;
+UPDATE item SET price = 65 WHERE itemId = 1028;
+UPDATE item SET price = 65 WHERE itemId = 1029;
+UPDATE item SET price = 54 WHERE itemId = 1018;
+UPDATE item SET price = 59 WHERE itemId = 1030;
+UPDATE item SET price = 69 WHERE itemId = 1031;
+UPDATE item SET price = 49 WHERE itemId = 1033;
+UPDATE item SET price = 49 WHERE itemId = 1032;
+UPDATE item SET price = 55 WHERE itemId = 1034;
+UPDATE item SET price = 39 WHERE itemId = 1035;
+UPDATE item SET price = 49 WHERE itemId = 1036;
+UPDATE item SET price = 76 WHERE itemId = 1037;
+UPDATE item SET price = 59 WHERE itemId = 1039;
+UPDATE item SET price = 65 WHERE itemId = 1038;
+UPDATE item SET price = 74 WHERE itemId = 1040;
+UPDATE item SET price = 69 WHERE itemId = 1041;
+UPDATE item SET price = 85 WHERE itemId = 1042;
+UPDATE item SET price = 65 WHERE itemId = 1043;
+UPDATE item SET price = 25 WHERE itemId = 1046;
+UPDATE item SET price = 45 WHERE itemId = 1057;
+UPDATE item SET price = 55 WHERE itemId = 1011;
+UPDATE item SET price = 69 WHERE itemId = 1013;
+UPDATE item SET price = 64 WHERE itemId = 1015;
+UPDATE item SET price = 79 WHERE itemId = 1017;
+UPDATE item SET price = 50 WHERE itemId = 1003;
+UPDATE item SET price = 64 WHERE itemId = 1005;
+UPDATE item SET price = 70 WHERE itemId = 1007;
+UPDATE item SET price = 64 WHERE itemId = 1009;
+UPDATE item SET price = 69 WHERE itemId = 1019;
+
+
+
+
+#Insert new menu items.
+
+INSERT INTO item (itemId, itemName, categoryId, price) VALUES (1061, 'Caramel Frappe', 102, 615.00);
+INSERT INTO item (itemId, itemName, categoryId, price) VALUES (1062, 'Green Apple Iced Tea', 102, 65.00);
+INSERT INTO item (itemId, itemName, categoryId, price) VALUES (1063, 'Bottled Water', 102, 12.00);
+INSERT INTO item (itemId, itemName, categoryId, price) VALUES (1064, 'Kiwi Lemonade (Soda)', 102, 49.00);
+INSERT INTO item (itemId, itemName, categoryId, price) VALUES (1065, 'Strawberry Lemonade (Soda)', 102, 42.00);
+INSERT INTO item (itemId, itemName, categoryId, price) VALUES (1066, 'Green Apple Soda', 102, 65.00);
+INSERT INTO item (itemId, itemName, categoryId, price) VALUES (1067, 'Blue Ocean', 102, 55.00);
+INSERT INTO item (itemId, itemName, categoryId, price) VALUES (1068, 'Vanilla', 102, 45.00);
+INSERT INTO item (itemId, itemName, categoryId, price) VALUES (1069, 'Mango', 102, 49.00);
+INSERT INTO item (itemId, itemName, categoryId, price) VALUES (1070, 'Pasta(Veg)', 103, 55.00);
+INSERT INTO item (itemId, itemName, categoryId, price) VALUES (1071, 'Tomato Cheese S/W WB', 103, 49.00);
+INSERT INTO item (itemId, itemName, categoryId, price) VALUES (1072, 'Coleslaw S/W WB', 103, 59.00);
+INSERT INTO item (itemId, itemName, categoryId, price) VALUES (1073, 'Corn Mix', 103, 39.00);
+INSERT INTO item (itemId, itemName, categoryId, price) VALUES (1074, 'Veg Puff', 103, 20.00);
+INSERT INTO item (itemId, itemName, categoryId, price) VALUES (1075, 'Chips', 103, 15.00);
+INSERT INTO item (itemId, itemName, categoryId, price) VALUES (1076, 'Pasta (Chicken)', 103, 59.00);
+INSERT INTO item (itemId, itemName, categoryId, price) VALUES (1077, 'Egg S/W WB', 103, 42.00);
+INSERT INTO item (itemId, itemName, categoryId, price) VALUES (1078, 'Egg Burger S/W WB', 103, 49.00);
+INSERT INTO item (itemId, itemName, categoryId, price) VALUES (1079, 'Eggless-Vanilla Cupcake', 105, 25.00);
+INSERT INTO item (itemId, itemName, categoryId, price) VALUES (1080, 'Eggless-Chocolate Cupcake', 105, 25.00);
+INSERT INTO item (itemId, itemName, categoryId, price) VALUES (1081, 'Vanilla Cupcake', 105, 25.00);
+INSERT INTO item (itemId, itemName, categoryId, price) VALUES (1082, 'Chocolate Cupcake', 105, 25.00);
+INSERT INTO item (itemId, itemName, categoryId, price) VALUES (1083, 'Chocolate Chip Cookie', 105, 15.00);
+INSERT INTO item (itemId, itemName, categoryId, price) VALUES (1084, 'Chocolate Chip Cookie(2)', 105, 25.00);
+INSERT INTO item (itemId, itemName, categoryId, price) VALUES (1085, 'Plain Waffle Large', 104, 49.00);
+INSERT INTO item (itemId, itemName, categoryId, price) VALUES (1086, 'Dry Fruit Waffle', 104, 35.00);
+INSERT INTO item (itemId, itemName, categoryId, price) VALUES (1087, 'Dry Fruit Waffle Large', 104, 69.00);
+INSERT INTO item (itemId, itemName, categoryId, price) VALUES (1088, 'Chocolate Waffle', 104, 33.00);
+INSERT INTO item (itemId, itemName, categoryId, price) VALUES (1089, 'Chocolate Waffle Large', 104, 65.00);
+INSERT INTO item (itemId, itemName, categoryId, price) VALUES (1090, 'Choco Chip Waffle', 104, 32.00);
+INSERT INTO item (itemId, itemName, categoryId, price) VALUES (1091, 'Choco Chip Waffle Large', 104, 62.00);
