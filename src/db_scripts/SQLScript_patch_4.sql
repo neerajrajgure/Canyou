c6b5d26e225c73b04c68e813a4c96d1774b510bb
SaNKeT <SaNKeT>
2016-05-16 08:05:51 +0530

    Updating Menu Item and Adding New Items final Update

--- src/db_scripts/SQLScript_patch_4.sql
+++ src/db_scripts/SQLScript_patch_4.sql
@@ -1,84 +1,84 @@
 #Update old items for price.
 
-UPDATE item SET price = 32 WHERE itemId = 1001;
-UPDATE item SET price = 40 WHERE itemId = 1010;
-UPDATE item SET price = 55 WHERE itemId = 1012;
-UPDATE item SET price = 49 WHERE itemId = 1014;
-UPDATE item SET price = 64 WHERE itemId = 1016;
-UPDATE item SET price = 35 WHERE itemId = 1002;
-UPDATE item SET price = 48 WHERE itemId = 1004;
-UPDATE item SET price = 55 WHERE itemId = 1006;
-UPDATE item SET price = 48 WHERE itemId = 1008;
-UPDATE item SET price = 39 WHERE itemId = 1020;
-UPDATE item SET price = 45 WHERE itemId = 1021;
-UPDATE item SET price = 47 WHERE itemId = 1022;
-UPDATE item SET price = 45 WHERE itemId = 1023;
-UPDATE item SET price = 55 WHERE itemId = 1024;
-UPDATE item SET price = 62 WHERE itemId = 1025;
-UPDATE item SET price = 65 WHERE itemId = 1026;
-UPDATE item SET price = 45 WHERE itemId = 1027;
-UPDATE item SET price = 65 WHERE itemId = 1028;
-UPDATE item SET price = 65 WHERE itemId = 1029;
-UPDATE item SET price = 54 WHERE itemId = 1018;
-UPDATE item SET price = 59 WHERE itemId = 1030;
-UPDATE item SET price = 69 WHERE itemId = 1031;
-UPDATE item SET price = 49 WHERE itemId = 1033;
-UPDATE item SET price = 49 WHERE itemId = 1032;
-UPDATE item SET price = 55 WHERE itemId = 1034;
-UPDATE item SET price = 39 WHERE itemId = 1035;
-UPDATE item SET price = 49 WHERE itemId = 1036;
-UPDATE item SET price = 76 WHERE itemId = 1037;
-UPDATE item SET price = 59 WHERE itemId = 1039;
-UPDATE item SET price = 65 WHERE itemId = 1038;
-UPDATE item SET price = 74 WHERE itemId = 1040;
-UPDATE item SET price = 69 WHERE itemId = 1041;
-UPDATE item SET price = 85 WHERE itemId = 1042;
-UPDATE item SET price = 65 WHERE itemId = 1043;
-UPDATE item SET price = 25 WHERE itemId = 1046;
-UPDATE item SET price = 45 WHERE itemId = 1057;
-UPDATE item SET price = 55 WHERE itemId = 1011;
-UPDATE item SET price = 69 WHERE itemId = 1013;
-UPDATE item SET price = 64 WHERE itemId = 1015;
-UPDATE item SET price = 79 WHERE itemId = 1017;
-UPDATE item SET price = 50 WHERE itemId = 1003;
-UPDATE item SET price = 64 WHERE itemId = 1005;
-UPDATE item SET price = 70 WHERE itemId = 1007;
-UPDATE item SET price = 64 WHERE itemId = 1009;
-UPDATE item SET price = 69 WHERE itemId = 1019;
+UPDATE item SET price = 32.00 WHERE itemId = 1001;#Espresso
+UPDATE item SET price = 40.00 WHERE itemId = 1010;#cappu nat
+UPDATE item SET price = 55.00 WHERE itemId = 1012;#cappu nat flav
+UPDATE item SET price = 49.00 WHERE itemId = 1014;#latte nat
+UPDATE item SET price = 64.00 WHERE itemId = 1016;#latte flav R
+UPDATE item SET price = 35.00 WHERE itemId = 1002;#americano R
+UPDATE item SET price = 48.00 WHERE itemId = 1004;#irish R
+UPDATE item SET price = 55.00 WHERE itemId = 1006;#mocha R
+UPDATE item SET price = 48.00 WHERE itemId = 1008;#flat white R
+UPDATE item SET price = 39.00 WHERE itemId = 1020;#regular tea
+UPDATE item SET price = 45.00 WHERE itemId = 1060;#masala tea
+UPDATE item SET price = 45.00 WHERE itemId = 1021;#darjeeling tea
+UPDATE item SET price = 47.00 WHERE itemId = 1022;#green tea
+UPDATE item SET price = 45.00 WHERE itemId = 1023;#iced coffee
+UPDATE item SET price = 55.00 WHERE itemId = 1024;#frappe
+UPDATE item SET price = 62.00 WHERE itemId = 1025;#creamy frappe
+UPDATE item SET price = 65.00 WHERE itemId = 1026;#mocha frappe
+UPDATE item SET price = 45.00 WHERE itemId = 1027;#affogato
+UPDATE item SET price = 65.00 WHERE itemId = 1028;#leamon iced tea
+UPDATE item SET price = 65.00 WHERE itemId = 1029;#peach iced tea
+UPDATE item SET price = 54.00 WHERE itemId = 1018;#hot chocolate R
+UPDATE item SET price = 59.00 WHERE itemId = 1030;#cold chocolate
+UPDATE item SET price = 69.00 WHERE itemId = 1031;#big o
+UPDATE item SET price = 49.00 WHERE itemId = 1033;#strawberry
+UPDATE item SET price = 49.00 WHERE itemId = 1032;#rose
+UPDATE item SET price = 55.00 WHERE itemId = 1034;#butterscotch
+UPDATE item SET price = 39.00 WHERE itemId = 1035;#cheese s/w
+UPDATE item SET price = 49.00 WHERE itemId = 1036;#corn cheese s/w
+UPDATE item SET price = 76.00 WHERE itemId = 1037;#club s/w
+UPDATE item SET price = 59.00 WHERE itemId = 1039;#chocolate s/w
+UPDATE item SET price = 65.00 WHERE itemId = 1038;#paneer s/w
+UPDATE item SET price = 74.00 WHERE itemId = 1040;#chicken mayo
+UPDATE item SET price = 69.00 WHERE itemId = 1041;#grilled chicken
+UPDATE item SET price = 85.00 WHERE itemId = 1042;#tandoori chicken
+UPDATE item SET price = 65.00 WHERE itemId = 1043;#bbq chicken
+UPDATE item SET price = 49.00 WHERE itemId = 1046;#plain waffle large
 
+UPDATE item SET price = 45.00 WHERE itemId = 1057;#large espresso
+UPDATE item SET price = 55.00 WHERE itemId = 1011;#large cappu
+UPDATE item SET price = 69.00 WHERE itemId = 1013;#large cappu flav
+UPDATE item SET price = 64.00 WHERE itemId = 1015;#latte nat large
+UPDATE item SET price = 79.00 WHERE itemId = 1017;#latte flav large
+UPDATE item SET price = 50.00 WHERE itemId = 1003;#large americano
+UPDATE item SET price = 64.00 WHERE itemId = 1005;#large irish
+UPDATE item SET price = 70.00 WHERE itemId = 1007;#large mocha
+UPDATE item SET price = 64.00 WHERE itemId = 1009;#large flat white
+UPDATE item SET price = 69.00 WHERE itemId = 1019;#large hot chocolate
 
-
+UPDATE item SET price = 65.00 WHERE itemId = 1067;#Green apple iced tea
+UPDATE item SET price = 39.00 WHERE itemId = 1078;#corn mix
+UPDATE item SET price = 15.00 WHERE itemId = 1079;#chips
+UPDATE item SET price = 12.00 WHERE itemId = 1072;#bottled water
+UPDATE item SET price = 49.00 WHERE itemId = 1068;#kiwi soda
+UPDATE item SET price = 42.00 WHERE itemId = 1069;#straw soda
+UPDATE item SET price = 65.00 WHERE itemId = 1070;#green apple
+UPDATE item SET price = 55.00 WHERE itemId = 1071;#blue ocean
+UPDATE item SET price = 45.00 WHERE itemId = 1064;#vanilla
+UPDATE item SET price = 49.00 WHERE itemId = 1065;#mango
+UPDATE item SET price = 49.00 WHERE itemId = 1075;#tomato cheese
+UPDATE item SET price = 59.00 WHERE itemId = 1073;#coleslaw
+UPDATE item SET price = 15.00 WHERE itemId = 1080;#chocolate cookies(1)
+UPDATE item SET price = 25.00 WHERE itemId = 1081;#chocolate cookies(2)
+UPDATE item SET price = 55.00 WHERE itemId = 1077;#veg pasta
 
 #Insert new menu items.
 
-INSERT INTO item (itemId, itemName, categoryId, price) VALUES (1061, 'Caramel Frappe', 102, 615.00);
-INSERT INTO item (itemId, itemName, categoryId, price) VALUES (1062, 'Green Apple Iced Tea', 102, 65.00);
-INSERT INTO item (itemId, itemName, categoryId, price) VALUES (1063, 'Bottled Water', 102, 12.00);
-INSERT INTO item (itemId, itemName, categoryId, price) VALUES (1064, 'Kiwi Lemonade (Soda)', 102, 49.00);
-INSERT INTO item (itemId, itemName, categoryId, price) VALUES (1065, 'Strawberry Lemonade (Soda)', 102, 42.00);
-INSERT INTO item (itemId, itemName, categoryId, price) VALUES (1066, 'Green Apple Soda', 102, 65.00);
-INSERT INTO item (itemId, itemName, categoryId, price) VALUES (1067, 'Blue Ocean', 102, 55.00);
-INSERT INTO item (itemId, itemName, categoryId, price) VALUES (1068, 'Vanilla', 102, 45.00);
-INSERT INTO item (itemId, itemName, categoryId, price) VALUES (1069, 'Mango', 102, 49.00);
-INSERT INTO item (itemId, itemName, categoryId, price) VALUES (1070, 'Pasta(Veg)', 103, 55.00);
-INSERT INTO item (itemId, itemName, categoryId, price) VALUES (1071, 'Tomato Cheese S/W WB', 103, 49.00);
-INSERT INTO item (itemId, itemName, categoryId, price) VALUES (1072, 'Coleslaw S/W WB', 103, 59.00);
-INSERT INTO item (itemId, itemName, categoryId, price) VALUES (1073, 'Corn Mix', 103, 39.00);
-INSERT INTO item (itemId, itemName, categoryId, price) VALUES (1074, 'Veg Puff', 103, 20.00);
-INSERT INTO item (itemId, itemName, categoryId, price) VALUES (1075, 'Chips', 103, 15.00);
-INSERT INTO item (itemId, itemName, categoryId, price) VALUES (1076, 'Pasta (Chicken)', 103, 59.00);
-INSERT INTO item (itemId, itemName, categoryId, price) VALUES (1077, 'Egg S/W WB', 103, 42.00);
-INSERT INTO item (itemId, itemName, categoryId, price) VALUES (1078, 'Egg Burger S/W WB', 103, 49.00);
-INSERT INTO item (itemId, itemName, categoryId, price) VALUES (1079, 'Eggless-Vanilla Cupcake', 105, 25.00);
-INSERT INTO item (itemId, itemName, categoryId, price) VALUES (1080, 'Eggless-Chocolate Cupcake', 105, 25.00);
-INSERT INTO item (itemId, itemName, categoryId, price) VALUES (1081, 'Vanilla Cupcake', 105, 25.00);
-INSERT INTO item (itemId, itemName, categoryId, price) VALUES (1082, 'Chocolate Cupcake', 105, 25.00);
-INSERT INTO item (itemId, itemName, categoryId, price) VALUES (1083, 'Chocolate Chip Cookie', 105, 15.00);
-INSERT INTO item (itemId, itemName, categoryId, price) VALUES (1084, 'Chocolate Chip Cookie(2)', 105, 25.00);
-INSERT INTO item (itemId, itemName, categoryId, price) VALUES (1085, 'Plain Waffle Large', 104, 49.00);
-INSERT INTO item (itemId, itemName, categoryId, price) VALUES (1086, 'Dry Fruit Waffle', 104, 35.00);
-INSERT INTO item (itemId, itemName, categoryId, price) VALUES (1087, 'Dry Fruit Waffle Large', 104, 69.00);
-INSERT INTO item (itemId, itemName, categoryId, price) VALUES (1088, 'Chocolate Waffle', 104, 33.00);
-INSERT INTO item (itemId, itemName, categoryId, price) VALUES (1089, 'Chocolate Waffle Large', 104, 65.00);
-INSERT INTO item (itemId, itemName, categoryId, price) VALUES (1090, 'Choco Chip Waffle', 104, 32.00);
-INSERT INTO item (itemId, itemName, categoryId, price) VALUES (1091, 'Choco Chip Waffle Large', 104, 62.00);
+
+INSERT INTO item (itemId, itemName, categoryId, price) VALUES (1091, 'Veg Puff', 103, 20.00);
+INSERT INTO item (itemId, itemName, categoryId, price) VALUES (1092, 'Pasta (Chicken)(Marinara)', 103, 59.00);
+INSERT INTO item (itemId, itemName, categoryId, price) VALUES (1093, 'Egg S/W WB', 103, 42.00);
+INSERT INTO item (itemId, itemName, categoryId, price) VALUES (1094, 'Egg Burger S/W WB', 103, 49.00);
+INSERT INTO item (itemId, itemName, categoryId, price) VALUES (1095, 'Eggless-Vanilla Cupcake', 105, 25.00);
+INSERT INTO item (itemId, itemName, categoryId, price) VALUES (1096, 'Eggless-Chocolate Cupcake', 105, 25.00);
+INSERT INTO item (itemId, itemName, categoryId, price) VALUES (1097, 'Vanilla Cupcake', 105, 25.00);
+INSERT INTO item (itemId, itemName, categoryId, price) VALUES (1098, 'Chocolate Cupcake', 105, 25.00);
+INSERT INTO item (itemId, itemName, categoryId, price) VALUES (1101, 'Plain Waffle Reg.', 104, 25.00);
+INSERT INTO item (itemId, itemName, categoryId, price) VALUES (1102, 'Dry Fruit Waffle Reg.', 104, 35.00);
+INSERT INTO item (itemId, itemName, categoryId, price) VALUES (1103, 'Dry Fruit Waffle Large', 104, 69.00);
+INSERT INTO item (itemId, itemName, categoryId, price) VALUES (1104, 'Chocolate Waffle Reg.', 104, 33.00);
+INSERT INTO item (itemId, itemName, categoryId, price) VALUES (1105, 'Chocolate Waffle Large', 104, 65.00);
+INSERT INTO item (itemId, itemName, categoryId, price) VALUES (1106, 'Choco Chip Waffle Reg.', 104, 32.00);
+INSERT INTO item (itemId, itemName, categoryId, price) VALUES (1107, 'Choco Chip Waffle Large', 104, 62.00);
