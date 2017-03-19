570d76b40d1357b22917036d25ac6480ace86808
sanketkapile <sanketkapile1090@gmail.com>
2017-03-19 12:21:08 +0530

    SQL script ready

---
+++ src/db_scripts/SQLScript_patch_9.sql
@@ -0,0 +1,23 @@
+INSERT INTO categories (categoryId,categoryName,imageIcon) VALUES(109,'Bombay Ice-Cream','./src/images/Bombay_IceCream.png');
+
+INSERT INTO item (itemId, itemName, categoryId, price, secCategoryId) VALUES(4001,'Malai (100 gems)',109,30,110);
+INSERT INTO item (itemId, itemName, categoryId, price, secCategoryId) VALUES(4002,'Pista (100 gems)',109,30,110);
+INSERT INTO item (itemId, itemName, categoryId, price, secCategoryId) VALUES(4003,'Mango (100 gems)',109,30,110);
+INSERT INTO item (itemId, itemName, categoryId, price, secCategoryId) VALUES(4004,'Chocolate (100 gems)',109,40,110);
+INSERT INTO item (itemId, itemName, categoryId, price, secCategoryId) VALUES(4005,'Gulkand (100 gems)',109,40,110);
+INSERT INTO item (itemId, itemName, categoryId, price) VALUES(4006,'Kasata (Dry Fruit) (100 gems)',109,50);
+
+
+INSERT INTO categories (categoryId,categoryName,imageIcon) VALUES(110,'Kulfi','./src/images/Kulfi.png');
+
+INSERT INTO item (itemId, itemName, categoryId, price, secCategoryId) VALUES(4101,'Malai',110,20);
+INSERT INTO item (itemId, itemName, categoryId, price, secCategoryId) VALUES(4102,'Pista',110,20);
+INSERT INTO item (itemId, itemName, categoryId, price, secCategoryId) VALUES(4103,'Mango',110,20);
+INSERT INTO item (itemId, itemName, categoryId, price, secCategoryId) VALUES(4104,'Chocolate',110,20);
+INSERT INTO item (itemId, itemName, categoryId, price, secCategoryId) VALUES(4105,'Gulkand',110,20);
+
+
+INSERT INTO categories (categoryId,categoryName,imageIcon) VALUES(111,'Ice-Cream','./src/images/Ice-Cream.png');
+
+INSERT INTO item (itemId, itemName, categoryId, price, secCategoryId) VALUES(4201,'Faluda Reg',111,35);
+INSERT INTO item (itemId, itemName, categoryId, price, secCategoryId) VALUES(4202,'Faluda Large',111,50);
