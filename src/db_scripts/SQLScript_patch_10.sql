718e9362288354395e4bba0eada5b1e105020aac
sanketkapile <sanketkapile1090@gmail.com>
2017-03-24 10:44:06 +0530

    SQL script for momos and rolls

---
+++ src/db_scripts/SQLScript_patch_10.sql
@@ -0,0 +1,8 @@
+INSERT INTO categories (categoryId, categoryName, imageIcon) VALUES(110, 'Momos n Rolls', './src/images/MomosNRolls.png');
+
+
+INSERT INTO item (itemId, itemName, categoryId, price) VALUES(4101, 'Veg. Momos', 110, 49);
+INSERT INTO item (itemId, itemName, categoryId, price) VALUES(4102, 'Non Veg. Momos', 110, 59); 
+INSERT INTO item (itemId, itemName, categoryId, price) VALUES(4103, 'Veg. Roll', 110, 49);
+INSERT INTO item (itemId, itemName, categoryId, price) VALUES(4104, 'Paneer Roll', 110, 59);
+INSERT INTO item (itemId, itemName, categoryId, price) VALUES(4105, 'Chicken Roll', 110, 69);
