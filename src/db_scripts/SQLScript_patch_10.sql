188088a50fda8c2e1cb935959d8124073abf940b
Neeraj Rajgure <neeraj@tumblr.com>
2017-04-01 21:10:52 -0400

    Wraps and momos menu

---
+++ src/db_scripts/SQLScript_patch_10.sql
@@ -0,0 +1,9 @@
+use hms;
+
+INSERT INTO categories (categoryId, categoryName, imageIcon) VALUES(110, 'Wraps and Momos', './src/images/Wraps_N_Momos.png');
+
+INSERT INTO item (itemId, itemName, categoryId, price) VALUES(4101, 'Veg. Momos', 110, 49);
+INSERT INTO item (itemId, itemName, categoryId, price) VALUES(4102, 'Chicken Momos', 110, 59);
+INSERT INTO item (itemId, itemName, categoryId, price) VALUES(4103, 'Veg. Wrap', 110, 49);
+INSERT INTO item (itemId, itemName, categoryId, price) VALUES(4104, 'Paneer Wrap', 110, 59);
+INSERT INTO item (itemId, itemName, categoryId, price) VALUES(4105, 'Chicken Wrap', 110, 69);
