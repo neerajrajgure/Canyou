837a82dbdf7a07b519695d321bba26750766baa6
Neeraj Rajgure <neeraj@tumblr.com>
2017-04-23 15:50:05 -0400

    Adding Juice, Idli, Vada and three more wraps.

---
+++ src/db_scripts/SQLScript_patch_11.sql
@@ -0,0 +1,16 @@
+
+/*Wraps Item*/
+INSERT INTO item (itemId, itemName, categoryId, price) VALUES(4124, 'Chili Potato Wrap', 110, 40);
+INSERT INTO item (itemId, itemName, categoryId, price) VALUES(4125, 'Veg Mayo Wrap', 110, 35);
+INSERT INTO item (itemId, itemName, categoryId, price) VALUES(4126, 'Egg Wrap', 110, 35);
+
+/*Juices (Cold Bevezrage)*/
+INSERT INTO item (itemId, itemName, categoryId, price) VALUES(4201, 'Orange Juice', 102, 30);
+INSERT INTO item (itemId, itemName, categoryId, price) VALUES(4202, 'Mosambi Juice', 102, 30);
+INSERT INTO item (itemId, itemName, categoryId, price) VALUES(4203, 'Pineapple Juice', 102, 30);
+INSERT INTO item (itemId, itemName, categoryId, price) VALUES(4204, 'Watermelon Juice', 102, 30);
+
+/*Breakfast*/
+INSERT INTO item (itemId, itemName, categoryId, price) VALUES(3051, 'Idli Sambar', 108, 20);
+INSERT INTO item (itemId, itemName, categoryId, price) VALUES(3052, 'Medu Vada Sambar', 108, 30);
+INSERT INTO item (itemId, itemName, categoryId, price) VALUES(3053, 'Mix Idli-MVada Smbr', 108, 25);
