f6c15cbaa3d3e88ed6799512e17970e5f99dbf74
Neeraj Rajgure <neeraj@tumblr.com>
2017-05-13 15:37:57 -0400

    Add Pav Bhaji to menu

---
+++ src/db_scripts/SQLScript_patch_12.sql
@@ -0,0 +1,9 @@
+
+/*Pav BhajiItem*/
+INSERT INTO item (itemId, itemName, categoryId, price) VALUES(4141, 'Extra Butter Pav', 110, 5);
+INSERT INTO item (itemId, itemName, categoryId, price) VALUES(4142, 'Extra Butter Pav Jodi', 110, 8);
+INSERT INTO item (itemId, itemName, categoryId, price) VALUES(4143, 'Butter Pav Bhaji', 110, 50);
+INSERT INTO item (itemId, itemName, categoryId, price) VALUES(4144, 'Spicy Pav Bhaji', 110, 55);
+INSERT INTO item (itemId, itemName, categoryId, price) VALUES(4145, 'Cheese Pav Bhaji', 110, 60);
+INSERT INTO item (itemId, itemName, categoryId, price) VALUES(4146, 'Special Pav Bhaji', 110, 65);
+
